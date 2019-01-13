15b4a427412aced2dee3e263d1b383041023f45e
Rahul Karmoker <karmoker.rahul4@gmail.com>
2019-01-13 20:04:54 +0600

    TO-LET

---
+++ DataLayer/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Renter> Renters { get; set; }
+    }
+}
---
+++ DataLayer/Owner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(30)]
+        public string Name { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        [Required, Phone]
+        public string Phone { get; set; }
+        [Required, MaxLength(50)]
+        public string Adress { get; set; }
+        [Required, MaxLength(20)]
+        public string Area { get; set; }
+    }
+}
